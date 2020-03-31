882202bb07210aaa79b2f7f1780792b3421fded7
derispratama <derisrplb15@gmail.com>
2020-03-31 15:33:19 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, eaque? Nobis voluptates non vel soluta, obcaecati mollitia fuga explicabo assumenda modi deleniti, voluptas minima dolorem tempora adipisci sed fugiat rerum.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
